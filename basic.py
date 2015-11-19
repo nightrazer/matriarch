8992daffeb5276bab238a8017451bba0842c72ec
nightrazer <nightrazer@gmail.com>
2015-11-19 21:18:34 +0100

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,10 @@
+print ("first lets try output on the screen!")
+myName = input("What is your name? ")
+print (myName)
+
+if(myName == "Simon"):
+    print ("Simon is the best!")
+elif(myName == "Henrik"):
+    print("Henrik is OK")
+else:
+    print("Hello world!")
