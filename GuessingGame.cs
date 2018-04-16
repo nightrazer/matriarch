84d5e790cf2fe97a2b7fdbb30edd8d0daa8f3620
nightrazer <nightrazer@gmail.com>
2018-04-16 15:26:56 +0200

    Update GuessingGame.cs

--- GuessingGame.cs
+++ GuessingGame.cs
@@ -1,150 +1,62 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System; // system måste anges som System
 
-namespace SimpleGames
+namespace Uppgift_4
 {
-    /*
-     * I koden ska det finnas exception handlers genom Try och flera Catch exceptions
-     * Exempelcis DivideByZeroExcepion e , Exception e
-     * 
-     * Du kan använda dig utav Int32.TryParse(Console.ReadLine(), out myNum
-     * Men det är inte bra att myNum = 0; och enbart kan köras en gång.
-     * Vi vill ha ett värde ifrån användaren!
-     * 
-     * Programmet ska beskrivas hur det är uppbyggt genom PseudoKod
-     * Använd Draw.io https://www.draw.io
-     * Strukturer markeras med versaler och fet sil (OM, ANNARS, SÅ LÄNGE)
-     * Villkor markeras med kursiv text (OM vilkor, SÅ LÄNGE det här är sant)
-     * Instruktioner markeras ine ut. (Skriv ut "Hej!")
-     * 
-     * Din uppgift blir sedan att se till att programmet kan köras utan körtidsfel.
-     * För detta ska du använda dig av TryParse för att hantera inmatning.
-     * 
-     * Checklista:
-     * 1) Felsök programmet
-     * 2) Kommenterat alla fel i koden
-     * 3) Skrivit och laddat upp pseudokod för min lösning
-     * 4) Svarat på frågan om körtidsfel
-     * 5) Laddat upp källkoden för mitt program
-     */
-
     class Program
     {
         static void Main(string[] args)
         {
-            bool displayMenu = true;
-            while (displayMenu == true)
-            { 
-                 displayMenu = MainMenu();
-            }
-        }
+            // Deklaration av variabler
+            Random slumpat = new Random(); // skapar ett random objekt
+            int speltal = slumpat.Next(1 , 20); // anropar Next metoden för att skapa ett slumptal mellan 1 och 20
+            // Lade till int speltal = slumpat.Next(1 , 20); istället för int speltal = slumpat.Next();
+            int Gissningar = 0;
+            bool Fel = true;
 
 
-        private static bool MainMenu()
-        {
-            Console.Clear();
-            Console.WriteLine("Chose an option: ");
-            Console.WriteLine("[1] Print Numbers");
-            Console.WriteLine("[2] Guessing Game");
-            Console.WriteLine("[3] View Hi-Scores");
-            Console.WriteLine("[4] Exit");
+            // läs på, vad är overload metoder? https://msdn.microsoft.com/en-us/library/system.random.next(v=vs.110).aspx
+            bool spela = true; // Variabel för att kontrollera om spelet ska fortsätta köras
 
-            string result = Console.ReadLine();
-            if (result == "1")
+            while (spela) // För att loppen ska köras är det nödvändigt att den är sann medans den kör
+                          // ändrade (!spela) till (spela)
             {
-                PrintNumbers();
-                return true;
-            }
-            else if (result == "2")
-            {
-                GuessingGame();
-                return true;
-            }
-            else if (result == "3")
-            {
-                HighScores();
-                return true;
-            }
-            else if (result == "4")
-            {
-                return false;
-            }
+                Console.Write("\n\tGissa på ett tal mellan 1 och 20: ");
+                int tal = Convert.ToInt32(Console.ReadLine());
 
-            else
-            {
-                return true;
-            }
-
-
-        }
-       
-
-        private static void PrintNumbers()
-        {
-            Console.Clear();
-            Console.WriteLine("Print numbers");
-            Console.Write("Guessing Game: ");
-            // Convert string to integer with Parse
-            int result = int.Parse(Console.ReadLine());
-            int counter = 1;
-            while (counter < result + 1)
-            {
-                Console.Write(counter);
-                Console.Write("-");
-                counter++;
-                
-            }
-            Console.ReadLine();
-        }
-        private static void GuessingGame()
-        {
-            Console.Clear();
-            Console.WriteLine("Guessing game!");
-
-            Random myRandom = new Random();
-            int randomNumber = myRandom.Next(1, 11);
-
-            int guesses = 0;
-            bool incorrect = true;
-
-            do
-            {
-                Console.WriteLine("Guess a number between 1 and 10: ");
-                string result = Console.ReadLine();
-                guesses++;
-                if (result == randomNumber.ToString())
-                incorrect = false;
-                else{
-                    Console.WriteLine("Wrong!");
+                if (tal < speltal)
+                {
+                   
+                    Console.WriteLine("\tDet inmatade talet " + tal + " är för litet, försök igen.");
+                    string resultat = Console.ReadLine();
+                    Gissningar++;
                 }
 
-            } while (incorrect);
-            Console.WriteLine("Correct! It took you {0} guesses.", guesses);
+                else if (tal > speltal) // ändrat till en else if
+                {
+                    Console.WriteLine("\tDet inmatade talet " + tal + " är för stort, försök igen.");
+                    string resultat = Console.ReadLine();
+                    Gissningar++;
+                    // Console.WriteLine("\tDet inmatade talet " + tal  " är för stort, försök igen.");
+                    // Strängen saknade ett plus för att addera ihop sista strängen
+                }
 
-    {
+                else if (tal == speltal) // (tal = speltal) är felaktigt angiven. Cannot convert int to bool
+                {
+                    // prövar (tal == speltal)
+                    // ändrat till en else if
+                    Console.WriteLine("\tGrattis, du gissade rätt!");
+                    Console.WriteLine("\tDet tog dig {0} gissningar", Gissningar);
+                    
+                    Gissningar++;
 
+                spela = false;
+
+                Console.ReadLine();
+                }
+                else{
+                    Console.WriteLine("Something wrong");
+                }
             }
-
-            Console.ReadLine();
-        }
-        private static void HighScores()
-        {
-            // Spara en highscore-lista som sparar antalet gissningar mellan flera spelomgångar
-            // Spara en lista på 10 Försök, Ange Datum
-            // Bonus: Spara highscore till en txt.fil som läses in av programmet vid start
-            // Bonus: Ge erbjudandet att Rensa Hi-score listan. 
-            // Exempelvis skriva över fil innehållet.
-            
-        }
-        private static void Player()
-        {
-            // Värdet på player ska nollställas för varje gång du spelar
-            // PlayerScore = 0; alterantivt PlayerScore(""); för reset av värdet
-            // Om spelaren skriver ett namn som redan finns, medela högsta tidigare
-            // resultat och datumet, säg Lycka till!
         }
     }
 }
